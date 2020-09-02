85a7d3dd625989bd753179638355c283345fb39c
rajitha <rajithasrinath94@gmail.com>
2020-09-02 21:13:23 +0530

    log items component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
 
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.js'
@@ -8,10 +10,14 @@ const App = () => {
     // init material js
     M.AutoInit();
   });
+
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
